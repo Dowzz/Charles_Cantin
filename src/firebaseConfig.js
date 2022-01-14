25af9b39ec8a64248f076d7a26350b86f22a5519
Dowzz <michel.barny@outlook.fr>
2022-01-14 11:06:29 +0100

    update database
    Signed-off-by: Dowzz <michel.barny@outlook.fr>

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,14 @@ import "firebase/compat/auth";
 import "firebase/storage";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDVpuZ9fm69yRBPt7yxl_FdMm4SkC4BoPQ",
-  authDomain: "auth-production-7b5a1.firebaseapp.com",
-  databaseURL: "https://auth-production-7b5a1-default-rtdb.firebaseio.com",
-  projectId: "auth-production-7b5a1",
-  storageBucket: "auth-production-7b5a1.appspot.com",
-  messagingSenderId: "291878134150",
-  appId: "1:291878134150:web:3178391690dd8c99cf8c07",
+  apiKey: "AIzaSyBTlsV1XXHf_5ot3pvpzJkdAWRW4-cZgM4",
+  authDomain: "cantin-7b1e8.firebaseapp.com",
+  databaseURL:
+    "https://cantin-7b1e8-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "cantin-7b1e8",
+  storageBucket: "cantin-7b1e8.appspot.com",
+  messagingSenderId: "74276192669",
+  appId: "1:74276192669:web:7e29fadab98b3532ed2f74",
 };
 
 firebase.initializeApp(firebaseConfig);
